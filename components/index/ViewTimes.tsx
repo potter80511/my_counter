dcded13f99045d88ef395a11ed33568f9c2d21ea
簡豪江 <potter80511@gmail.com>
2020-05-31 16:05:31 +0800

    加上「計時中」閃爍功能

--- components/index/ViewTimes.tsx
+++ components/index/ViewTimes.tsx
@@ -1,7 +1,9 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React from 'react';
 import { StartStatus } from '../../types/counter';
 import { Spring } from 'react-spring/renderprops.cjs';
 import { CSSTransition } from 'react-transition-group';
+import { faStopwatch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '@styles/components/ViewTimes.scss';
 import '@styles/transition_group.scss';
 
@@ -27,7 +29,6 @@ const ViewTimes = (props: ViewTimesProps) => {
     viewSeconds,
     countingStatus,
   } = props;
-  const circleSvg = useRef(null);
 
   const height = 300;
   const r = (height - 10 ) / 2;
@@ -46,7 +47,7 @@ const ViewTimes = (props: ViewTimesProps) => {
   return (
     <div className="view-times" style={{height: circleHeight}}>
       <div className="circle" style={{height: circleHeight}}>
-        <svg ref={circleSvg} height="100%" width="100%">
+        <svg height="100%" width="100%" className="circleSvg">
           <circle cx={circleHeight/2} cy={circleHeight/2} r={circleRadius} stroke="#222" strokeWidth="7" fill="none" />
           { resetCircle ? (
             <Spring
@@ -62,18 +63,24 @@ const ViewTimes = (props: ViewTimesProps) => {
             <circle  cx={circleHeight/2} cy={circleHeight/2} r={circleRadius} stroke={circleStrokeColor} strokeWidth={circleStrokeWidth} strokeDasharray={circleLength} strokeDashoffset={countingStatus === StartStatus.pause ? circlePassedLength : circleLength} fill="none" strokeLinecap="round" />
           )}
         </svg>
-        <p className={`time${fontBigClass}`}>
-          <CSSTransition
-            in={showViewHours}
-            classNames='fade'
-            timeout={300}
-            unmountOnExit
-          >
-            <span>{viewHours}：</span>
-          </CSSTransition>
-          <span>{viewMinutes}：</span>
-          <span>{viewSeconds}</span>
-        </p>
+        <div className="counting">
+          <span className="label fadeInfinite">
+            <FontAwesomeIcon icon={faStopwatch} />
+            計時中..
+          </span>
+          <p className={`time${fontBigClass}`}>
+            <CSSTransition
+              in={showViewHours}
+              classNames='fade'
+              timeout={300}
+              unmountOnExit
+            >
+              <span>{viewHours}：</span>
+            </CSSTransition>
+            <span>{viewMinutes}：</span>
+            <span>{viewSeconds}</span>
+          </p>
+        </div>
       </div>
     </div>
   );
