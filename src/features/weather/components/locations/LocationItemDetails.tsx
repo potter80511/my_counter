16260bd349417cbbf9217d72f751be0a391673f0
簡豪江 <potter80511@gmail.com>
2020-07-05 15:44:03 +0800

    完成detail的滾動fixed 效果

--- src/features/weather/components/locations/LocationItemDetails.tsx
+++ src/features/weather/components/locations/LocationItemDetails.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import '@styles/transition_group.scss';
 import { CSSTransition } from 'react-transition-group';
 import { CurrentDayDetails } from 'src/features/weather/domain/model/Weather';
@@ -19,9 +19,39 @@ const LocationItemDetails = (props: LocationItemDetailsProps) => {
     onCloseSpread,
   } = props;
 
-  const everyTimeItem = locationData.todayEveryHourArray.map(item => {
+  const [everyTimeFixed, setTodayEveryTimeFixed] = useState<boolean>(false);
+  const [opacityValue, setOpacityValue] = useState<number>(1);
+  const [todayEveryTimeHeight, setTodayEveryTimeHeight] = useState<number>(0);
+  const fixedDistance = 130 - 7 - 20;
+  const opacityDistance = 100;
+
+  const contentRef = useRef(null);
+  const todayEveryTimeRef = useRef(null);
+
+  const onWeekScroll = () => {
+    const scrollTop = contentRef.current.scrollTop;
+    const opacityRate = 1 - (scrollTop / opacityDistance);
+    const opacity = opacityRate >= 0 ? opacityRate : 0;
+    setOpacityValue(opacity);
+    if (scrollTop >= fixedDistance) {
+      setTodayEveryTimeFixed(true)
+      setTodayEveryTimeHeight(todayEveryTimeRef.current.clientHeight);
+    } else {
+      setTodayEveryTimeFixed(false)
+    };
+  };
+
+  const todayEveryTimePosition = everyTimeFixed ? 'fixed' : 'unset';
+  const fixedBackgroundColor = everyTimeFixed ? 'rgba(0, 0, 0, .5)' : 'none';
+  const todayEveryTimeTop = everyTimeFixed ? ((translateD + todayEveryTimeHeight + 42) + 'px') : 'auto';
+  const morePaddingTop = everyTimeFixed ? 143 + todayEveryTimeHeight : 143;
+
+  const everyTimeItem = locationData.todayEveryHourArray.map((item, index) => {
     return (
-      <div className="item">
+      <div
+        className="item"
+        key={'location-item-' + index}
+      >
         <span className="hour">{item.hourName}</span>
         <span className="t">{item.temperature}</span>
       </div>
@@ -36,24 +66,40 @@ const LocationItemDetails = (props: LocationItemDetailsProps) => {
     >
       <div className="location-details">
         <div className="bg"></div>
-        <div className="content">
-          <div className="location-wx" style={{top: translateD + 'px'}}>
+        <div className="content" onScroll={onWeekScroll} ref={contentRef}>
+          <div className="location-wx" style={{top: (translateD + 15) + 'px', background: fixedBackgroundColor}}>
             <h2>{locationData.locationName}</h2>
             <span className="wx">{locationData.wX}</span>
-            <span className="current-temperature">{locationData.currentTemperature}</span>
+            <span
+              className="current-temperature"
+              style={{ opacity: opacityValue }}
+            >
+              {locationData.currentTemperature}
+            </span>
           </div>
-          <div className="more">
-            <div className="today">
+          <div className="more" style={{ paddingTop: morePaddingTop }}>
+            <div className="today" style={{ opacity: opacityValue }}>
               <div className="day">
                 <span className="day-name">星期六</span>
+                <span className="today-today">今天</span>
               </div>
               <div className="temperature-range">
                 <span className="max-t">27</span>
                 <span className="min-t">25</span>
               </div>
             </div>
-            <div className="today-every-time">
+            <div
+              className="today-every-time"
+              ref={todayEveryTimeRef}
+              style={{
+                position: todayEveryTimePosition,
+                top: todayEveryTimeTop,
+              }}
+            >
               {everyTimeItem}
+            </div>
+            <div className="week-weather">
+              asdfasdf
             </div>
           </div>
         </div>
