9cca9bb6c9ee7722b9de3832bad7e7b957000800
簡豪江 <potter80511@gmail.com>
2020-07-12 14:38:59 +0800

    新增台北、桃園、新北、宜蘭的location資料

--- src/features/weather/domain/data/allCitiesSeriesNumberData.ts
+++ src/features/weather/domain/data/allCitiesSeriesNumberData.ts
@@ -20,11 +20,71 @@ export const currentDayCitiesSeriesNumberData: SeriesNumberItem[] = [
     seriesNumber: SeriesNumber.Taoyuan,
   },
   {
+    name: TaiwanCities.Hsinchu,
+    seriesNumber: SeriesNumber.Hsinchu,
+  },
+  {
+    name: TaiwanCities.Miaoli,
+    seriesNumber: SeriesNumber.Miaoli,
+  },
+  {
+    name: TaiwanCities.Changhua,
+    seriesNumber: SeriesNumber.Changhua,
+  },
+  {
+    name: TaiwanCities.Nantou,
+    seriesNumber: SeriesNumber.Nantou,
+  },
+  {
+    name: TaiwanCities.Yunlin,
+    seriesNumber: SeriesNumber.Yunlin,
+  },
+  {
+    name: TaiwanCities.Chiayi,
+    seriesNumber: SeriesNumber.Chiayi,
+  },
+  {
+    name: TaiwanCities.Pingtung,
+    seriesNumber: SeriesNumber.Pingtung,
+  },
+  {
+    name: TaiwanCities.Taitung,
+    seriesNumber: SeriesNumber.Taitung,
+  },
+  {
+    name: TaiwanCities.Hualien,
+    seriesNumber: SeriesNumber.Hualien,
+  },
+  {
+    name: TaiwanCities.Penghu,
+    seriesNumber: SeriesNumber.Penghu,
+  },
+  {
+    name: TaiwanCities.Keelung,
+    seriesNumber: SeriesNumber.Keelung,
+  },
+  {
     name: TaiwanCities.Taipei,
     seriesNumber: SeriesNumber.Taipei,
   },
   {
+    name: TaiwanCities.Kaohsiung,
+    seriesNumber: SeriesNumber.Kaohsiung,
+  },
+  {
     name: TaiwanCities.NewTaipei,
     seriesNumber: SeriesNumber.NewTaipei,
+  },
+  {
+    name: TaiwanCities.Taichung,
+    seriesNumber: SeriesNumber.Taichung,
+  },
+  {
+    name: TaiwanCities.Tainan,
+    seriesNumber: SeriesNumber.Tainan,
+  },
+  {
+    name: TaiwanCities.Kinmen,
+    seriesNumber: SeriesNumber.Kinmen,
   },
 ];
