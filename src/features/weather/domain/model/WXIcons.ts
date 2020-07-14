e9b7040f486311ff7a3ab43d54d852067420863a
簡豪江 <potter80511@gmail.com>
2020-07-14 23:16:24 +0800

    更新圖片

--- src/features/weather/domain/model/WXIcons.ts
+++ src/features/weather/domain/model/WXIcons.ts
@@ -5,5 +5,9 @@ export enum WXIcons {
   Cloudy = 'cloudy',
   CloudySunny = 'cloudy_sunny',
   CloudyTempRainyOrThunder = 'cloudy_temp_rainy_or_thunder',
+  CloudCloudyTempRain = 'cloud_cloudy_temp_rain',
+  CloudTempRain = 'cloud_temp_rain',
+  CloudyAfternoonTempRainyOrThunder = 'cloudy_afternoon_temp_rainy_or_thunder',
   TempRainyOrThunder = 'temp_rainy_or_thunder',
+  AfternoonTempRainyOrThunder = 'afternoon_temp_rainy_or_thunder',
 };
--- src/features/weather/domain/model/Weather.ts
+++ src/features/weather/domain/model/Weather.ts
@@ -6,8 +6,12 @@ export enum WXType {
   StrongCloudy = '陰',
   Cloudy = '多雲',
   CloudySunny = '多雲時晴',
-  CloudyTempRainyOrThunder = '多雲午後短暫雷陣雨',
-  TempRainyOrThunder = '午後短暫雷陣雨',
+  CloudyTempRainyOrThunder = '多雲短暫陣雨或雷雨',
+  CloudCloudyTempRain = '多雲時陰短暫陣雨',
+  CloudTempRain = '多雲短暫陣雨',
+  CloudyAfternoonTempRainyOrThunder = '多雲午後短暫雷陣雨',
+  TempRainyOrThunder = '短暫陣雨或雷雨',
+  AfternoonTempRainyOrThunder = '午後短暫雷陣雨',
 }
 
 export interface TodayEveryHour {
