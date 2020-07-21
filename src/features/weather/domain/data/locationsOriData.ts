fada37ade7e1fe2e58100dc470fe10d6d338ab81
簡豪江 <potter80511@gmail.com>
2020-07-21 17:56:19 +0800

    新增宜蘭ori資料

--- src/features/weather/domain/data/locationsOriData.ts
+++ src/features/weather/domain/data/locationsOriData.ts
@@ -19,12 +19,52 @@ export const locationsOriData: OriLocation[] = [
     city: TaiwanCities.Yilan,
     districts: [
       {
+        name: YilanLocationValue.YilanCity,
+        zip: '260',
+      },
+      {
         name: YilanLocationValue.LuoDong,
         zip: '265',
       },
       {
-        name: YilanLocationValue.YilanCity,
-        zip: '260',
+        name: YilanLocationValue.Suao,
+        zip: '270',
+      },
+      {
+        name: YilanLocationValue.Toucheng,
+        zip: '261',
+      },
+      {
+        name: YilanLocationValue.Jiaoxi,
+        zip: '262',
+      },
+      {
+        name: YilanLocationValue.ZhuangWai,
+        zip: '263',
+      },
+      {
+        name: YilanLocationValue.Yuanshan,
+        zip: '264',
+      },
+      {
+        name: YilanLocationValue.Dongshan,
+        zip: '269',
+      },
+      {
+        name: YilanLocationValue.Wujie,
+        zip: '268',
+      },
+      {
+        name: YilanLocationValue.Sunshin,
+        zip: '266',
+      },
+      {
+        name: YilanLocationValue.Datong,
+        zip: '267',
+      },
+      {
+        name: YilanLocationValue.NanAo,
+        zip: '272',
       },
     ],
   },
