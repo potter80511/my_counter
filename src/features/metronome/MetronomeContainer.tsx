12ed3a592ba1adcab3fe846d7201aa86db56bcea
簡豪江 <potter80511@gmail.com>
2020-10-27 12:29:43 +0800

    完成adjust-tool的style

--- src/features/metronome/MetronomeContainer.tsx
+++ src/features/metronome/MetronomeContainer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Screen from 'src/features/metronome/components/Screen';
+import AdjustingTool from 'src/features/metronome/components/AdjustingTool';
 import '@styles/features/metronome/metronome.scss';
 
 const MetronomeContainer = () => {
@@ -7,6 +8,12 @@ const MetronomeContainer = () => {
     <div className="metronome">
       <div className="metronome-head">
         <Screen />
+      </div>
+      <div className="metronome-body">
+        <div className="adjusting-tools">
+          <AdjustingTool label="速度" />
+          <AdjustingTool label="拍子" />
+        </div>
       </div>
     </div>
   );
