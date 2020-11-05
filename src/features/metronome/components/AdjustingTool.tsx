e18a01d6adc5de0733e21bbb320e1ee78c464a4b
簡豪江 <potter80511@gmail.com>
2020-11-05 17:58:44 +0800

    速度及拍好調整驗證改寫到reducer

--- src/features/metronome/components/AdjustingTool.tsx
+++ src/features/metronome/components/AdjustingTool.tsx
@@ -1,44 +1,23 @@
 import React from 'react';
 import '@styles/features/metronome/AdjustingTool.scss';
-import { AddReduce } from 'src/domain/model/AddReduce';
 
 type AdjustingToolProp = {
   label: string;
-  minValue: number;
-  maxValue: number;
   currentValue: number;
   onClick?: (newValue: number) => void;
 };
 
 const AdjustingTool = (props: AdjustingToolProp) => {
-  const { label, minValue, maxValue, currentValue, onClick } = props;
+  const { label, currentValue, onClick } = props;
 
   return (
     <div className="adjusting-tool">
       <label>{label}</label>
       <div className="tool-buttons">
-        <button
-          type="button"
-          onClick={() => {
-            if (currentValue < maxValue) {
-              onClick(currentValue + 1);
-            } else {
-              console.log('is max!');
-            }
-          }}
-        >
+        <button type="button" onClick={() => onClick(currentValue + 1)}>
           ＋
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            if (currentValue > minValue) {
-              onClick(currentValue - 1);
-            } else {
-              console.log('is min!');
-            }
-          }}
-        >
+        <button type="button" onClick={() => onClick(currentValue - 1)}>
           －
         </button>
       </div>
--- src/features/metronome/slices/settingSlice.ts
+++ src/features/metronome/slices/settingSlice.ts
@@ -24,13 +24,35 @@ export const { actions, reducer } = createSlice<State, CaseReducer>({
   initialState: defaultState,
   reducers: {
     loaded: (_state, action) => action.payload,
-    update: (state, action: PayloadAction<Partial<State>>) => ({
-      ...state,
-      ...action.payload,
-    }),
+    update: (state, action: PayloadAction<Partial<State>>) => {
+      const { speed } = action.payload;
+      if (speed < 30) {
+        console.log('已達最低速度30');
+        return;
+      }
+      if (speed > 300) {
+        console.log('已達最高速度300');
+        return;
+      }
+
+      return {
+        ...state,
+        ...action.payload,
+      };
+    },
     adjustedTimeSignature: (state, action: PayloadAction<number>) => {
+      const newIndex = action.payload;
+      const maxIndex = timeSignatureData.length - 1;
+      if (newIndex > maxIndex) {
+        console.log('已是最後一個！');
+        return;
+      }
+      if (newIndex < 0) {
+        console.log('已是第一個！');
+        return;
+      }
       const newTimeSignature = timeSignatureData.find(
-        (_item, index) => index === action.payload,
+        (_item, index) => index === newIndex,
       );
       return {
         ...state,
