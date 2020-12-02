b5fdd25346b6117678c1053e59b45b7fb8e52544
簡豪江 <potter80511@gmail.com>
2020-12-02 17:58:46 +0800

    加上beating時的綠色三角形判斷

--- src/features/metronome/slices/beatingSlice.ts
+++ src/features/metronome/slices/beatingSlice.ts
@@ -2,14 +2,17 @@ import { createSlice, PayloadAction, Action } from '@reduxjs/toolkit';
 
 export type State = {
   beatNumber: number;
+  startStatus: boolean;
 };
 
 export const defaultState: State = {
   beatNumber: 0,
+  startStatus: false,
 };
 
 export type CaseReducer = {
-  beat: (state: State, action: PayloadAction<State>) => State;
+  beat: (state: State, action: PayloadAction<number>) => State;
+  statusChanged: (state: State, action: PayloadAction<boolean>) => State;
   reset: (state: State, action: Action) => State;
 };
 
@@ -17,7 +20,18 @@ export const { actions, reducer } = createSlice<State, CaseReducer>({
   name: 'metronome/beating/',
   initialState: defaultState,
   reducers: {
-    beat: (_state, action) => action.payload,
+    beat: (state, action) => {
+      return {
+        ...state,
+        beatNumber: action.payload,
+      };
+    },
+    statusChanged: (state, action) => {
+      return {
+        ...state,
+        startStatus: action.payload,
+      };
+    },
     reset: () => defaultState,
   },
 });
