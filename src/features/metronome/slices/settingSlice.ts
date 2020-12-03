a56c420231684079fb3e9e5c85fcb2f21b317857
簡豪江 <potter80511@gmail.com>
2020-12-03 17:26:13 +0800

    修改速度驗證的提示文字

--- src/features/metronome/slices/settingSlice.ts
+++ src/features/metronome/slices/settingSlice.ts
@@ -33,6 +33,9 @@ export type CaseReducer = {
   reset: (state: State, action: Action) => State;
 };
 
+const minSpeed = 30;
+const maxSpeed = 240;
+
 export const { actions, reducer } = createSlice<State, CaseReducer>({
   name: 'metronome/setting/',
   initialState: defaultState,
@@ -71,14 +74,14 @@ export const { actions, reducer } = createSlice<State, CaseReducer>({
         return {
           ...state,
           speed: state.originalSpeed,
-          errorMessages: '已達最低速度30',
+          errorMessages: `不得低於最低速度 ${minSpeed}`,
         };
       }
-      if (numSpeed > 300) {
+      if (numSpeed > maxSpeed) {
         return {
           ...state,
           speed: state.originalSpeed,
-          errorMessages: '已達最高速度300',
+          errorMessages: `不得高於最高速度 ${maxSpeed}`,
         };
       }
       return {
